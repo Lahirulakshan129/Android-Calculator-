e1d5307875c241d660fdc935608312cf91709520
lahirulakshan129 <lahirulakshan129@gmail.com>
2024-11-23 12:49:50 +0530

    Created UI Buttons

--- app/src/main/java/com/myapp/calculator/Calculator.kt
+++ app/src/main/java/com/myapp/calculator/Calculator.kt
@@ -5,18 +5,34 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-value buttonList = listOf(
+val buttonList = listOf(
+    listOf("C", "(", ")", "/"),
+    listOf("7", "8", "9", "*"),
+    listOf("4", "5", "6", "-"),
+    listOf("1", "2", "3", "+"),
+    listOf("AC","0", ".", "=")
+)
 
 @Composable
 fun Calculator(modifier: Modifier = Modifier) {
@@ -34,6 +50,8 @@ fun Calculator(modifier: Modifier = Modifier) {
                 maxLines = 5,
                 overflow = TextOverflow.Ellipsis
             )
+            Spacer(modifier = Modifier.weight(1f))
+
             Text(
                 text="246",
                 style = TextStyle(
@@ -43,6 +61,49 @@ fun Calculator(modifier: Modifier = Modifier) {
                 maxLines = 2,
             )
             Spacer(modifier = Modifier.height(10.dp))
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(4)
+            ) {
+                items(buttonList.flatten()) { btn ->
+                    CalculatorButton(btn = btn)
+                }
+            }
+
         }
     }
+}
+@Composable
+fun CalculatorButton( btn : String){
+    Box(modifier = Modifier.padding(8.dp)){
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(80.dp),
+            shape= CircleShape,
+            contentColor = Color.White,
+            containerColor = getColor(btn)
+        )  {
+            Text(
+                text = btn,
+                fontSize = 22.sp,
+                fontWeight= FontWeight.Bold
+            )
+        }
+    }
+}
+
+fun getColor(btn: String): Color{
+    if (btn=="C" || btn=="AC"){
+        return Color(0xFF8C7162)
+    }
+    if (btn=="+" || btn=="-" || btn=="*" || btn=="/"){
+        return Color.Gray
+    }
+    if (btn=="="){
+        return Color(0xFF008FFF)
+    }
+    if (btn=="(" || btn==")"){
+        return Color.LightGray
+    }
+    return Color.DarkGray
+
 }
