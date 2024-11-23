6bd059aa3dbb030549f4e21322636165c3e44b35
lahirulakshan129 <lahirulakshan129@gmail.com>
2024-11-23 11:30:43 +0530

    Project created

--- app/src/main/java/com/myapp/calculator/Calculator.kt
+++ app/src/main/java/com/myapp/calculator/Calculator.kt
@@ -2,12 +2,21 @@ package com.myapp.calculator
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+value buttonList = listOf(
 
 @Composable
 fun Calculator(modifier: Modifier = Modifier) {
@@ -16,7 +25,24 @@ fun Calculator(modifier: Modifier = Modifier) {
             modifier = modifier.fillMaxSize(),
             horizontalAlignment = Alignment.End
         ){
-            Text(text="123+123")
+            Text(
+                text="123+123",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.End
+                ),
+                maxLines = 5,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text="246",
+                style = TextStyle(
+                    fontSize = 60.sp,
+                    textAlign = TextAlign.End
+                ),
+                maxLines = 2,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
